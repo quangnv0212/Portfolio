80cff3c657924e85e90a5afb70858e0ea9f685b3
Vu Quang <quangnv.0212@gmail.com>
2023-03-21 02:03:18 +0700

    Update CV and Project

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, imgUrl, linkDemo }) => {
     return (
         <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx">
-                <img style={{ width: '100%', height: '100%' }} src={imgUrl} />
+                <img style={{ width: '100%', height: '300px' }} src={imgUrl} />
                 <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
